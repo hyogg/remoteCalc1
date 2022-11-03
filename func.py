131c90aa33cab9fe5b320625d09c4d7dd5d1ccca
hyogg <itshyoj@gmail.com>
2022-11-03 15:21:44 +0900

    commit3 func 추가 완성

--- func.py
+++ func.py
@@ -2,4 +2,17 @@
 def add_func(n1, n2) :
     return n1+n2
 
+def sub_func(n1, n2) :
+    return n1-n2
 
+def mul_func(n1, n2) :
+    return n1*n2
+
+def div_func(n1, n2) :
+    return n1/n2
+
+def rem_func(n1, n2) :
+    return n1//n2
+
+def pow_func(n1, n2) :
+    return n1**n2
